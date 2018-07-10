88dab76bb6d095a33b6985c3083d0562b3113d0a
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-10 14:11:52 +0800

    p2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,23 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int klass;
+
+    public String introduce(){
+        return "I am a Student. I am at Class " + this.klass +".";
+    }
+
+    public Student(String name,int age,int klass){
+        super(name,age);
+        setKlass(klass);
+    }
+
 }
