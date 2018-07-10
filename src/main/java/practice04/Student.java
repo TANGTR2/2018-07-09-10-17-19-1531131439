b0d41dba21c9da1237c96e5c3bfffd5239ee02df
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-10 14:39:58 +0800

    p4

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,22 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int klass;
+
+    public String introduce(){
+        return super.introduce().concat(" I am a Student. I am at Class " + this.klass +".");
+    }
+
+    public Student(String name,int age,int klass){
+        super(name,age);
+        setKlass(klass);
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,10 @@
 package practice04;
-
-public class Worker {
+import java.util.*;
+public class Worker extends Person{
+    public Worker(String name,int age){
+        super(name,age);
+    }
+    public String introduce(){
+        return super.introduce().concat(" I am a Worker. I have a job.");
+    }
 }
