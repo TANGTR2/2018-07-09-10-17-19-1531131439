31157fcfbc6917ee66c57c238a08bb7979912c57
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-10 15:33:33 +0800

    p7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,20 @@
 package practice07;
 
 public class Klass {
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int number;
+    public String getDisplayName(){
+        return "Class "+this.getNumber();
+    }
+
+    public Klass(int number){
+        setNumber(number);
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,21 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass klass;
+    public String introduce(){
+        return super.introduce().concat(" I am a Student. I am at Class " + this.klass.getNumber() +".");
+    }
+
+    public Student(String name,int age,Klass klass){
+        super(name,age);
+        setKlass(klass);
+    }
 }
