2d11103436bab43ff7f302566d9d617538450b3a
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-10 16:13:25 +0800

    p8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,36 @@
 package practice08;
 
 public class Klass {
+    public Student getLeader() {
+        return leader;
+    }
+
+//    public void setLeader(String leader) {
+//        this.leader = leader;
+//    }
+
+    public Student leader;
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int number;
+    public String getDisplayName(){
+        return "Class "+this.getNumber();
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass().getNumber() == this.getNumber()){
+            leader = student;
+        }
+    }
+
+    public Klass(int number){
+        setNumber(number);
+    }
 }
+
